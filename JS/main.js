f07c746e49a50072776c615915b39d172b21a03c
Agustina <palmarocchiagustina@gmail.com>
2022-09-05 22:39:06 -0300

    revision

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,15 @@
 //pre-entrega
+
+//bienvenida
+function nombreUsuario() {
+    alert("Bienvenidos a CHILL OUT");
+    let nombreIngresado = prompt("Ingresa tu nombre");
+  
+      alert("Hola " + nombreIngresado + " estos son nuestros pruductos");
+    
+}
+
+nombreUsuario();
 //funcion generica para push
 
 function cargarProductos(arr, valor) {
@@ -32,11 +43,9 @@ function Pagos(forma, descuento) {
 }
 
 const pago1 = new Pagos ("crédito", 20);
-const pago2 = new Pagos ("efectivo", 15);
-
-
-
-
+const pago2 = new Pagos ("débito", 20);
+const pago3 = new Pagos ("transferencia", 15);
+const pago4 = new Pagos ("efectivo", 15);
 
 
 function Carro (productos, subtotal, formas, total) {
@@ -48,8 +57,19 @@ function Carro (productos, subtotal, formas, total) {
 
     
 }
+ 
+//BIENVENIDA
 
 
+function nombreUsuario() {
+    alert("Bienvenidos a CHILL OUT");
+    let nombreIngresado = prompt("Ingresa tu nombre");
+  
+      alert("Hola " + nombreIngresado + " estos son nuestros pruductos");
+    
+}
+
+nombreUsuario();
 
 // MENU para seleccion de prendas y q se haga el push al carrito
 
@@ -66,46 +86,42 @@ function seleccion() {
                 
                 carrito.push(prenda1);
                 console.log(carrito);
-               
-               
                 break;
             case "2":
                 carrito.push(prenda2);
                 console.log(carrito);
-                
-               
                 break;
             case "3":
                 carrito.push(prenda3);
                 console.log(carrito);
-                
-               
                 break;
             case "4":
                 carrito.push(prenda4);
                 console.log(carrito);
-                
+                break;
+                case "5":
+                    carrito.push(prenda4);
+                    console.log(carrito);
+                    break;
+            case "6":
+                    alert("Salir")
+                    break;
+            default: 
+                    alert("Ingresa una opción válida");
+                    break;
+               }
+        
+            eleccion = prompt("Ingresa el número de la prenda que eligas: \n 1-remera $2000\n 2-buzo $6000\n 3-jeans $10000\n 4- camisaco $12000 \n 5-zapatillas $20000 \n 6-SALIR ")
                
-                break;
-            case "5":
-                carrito.push(prenda5);
-                console.log(carrito);
-                break;
         
-            default:
-                alert("Ingresa opción válida");
-                break;
+           } while (eleccion != "6" & eleccion != 6);
+        //respuesta con el total
+           alert("Gracias por tu compra" );
+          
+           
         }
-
     
-     eleccion = prompt("Ingresa el número de la prenda que eligas: \n 1-remera $2000\n 2-buzo $6000\n 3-jeans $10000\n 4- camisaco $12000 \n 5-zapatillas $20000 \n 6-SALIR ")
-        
-
-    } while (eleccion != 6 && eleccion != "6");
     
-   /* return resultado = alert("El total de tu compra es de " + suma);*/
-    
-}
 
 seleccion();
 
@@ -115,7 +131,6 @@ const subTotal = carrito.reduce((acc, el) => {
 
 return acc + el.precio;
 
-
 },0);
 
 alert ("El sub-total de tu compra es " + subTotal )
@@ -123,160 +138,42 @@ carrito.push(subTotal);
 console.log(carrito);
 
 
-// MENU (FORMAS DE PAGO) 
+//filtrar forma de pago 
 
-function fPagos() {
-    let  pagos = prompt ("Selecciona la forma de pago, ingresando el número correspondiente : \n 1-Crédito \n 2-Debito \n 3-Efectivo o transferencia");
-    
-    do {
-        switch (pagos) {
-            case "1":
-                carrito.push(pago1)
-                alert ("Débito, esta opción tiene 20% de recargo.");
-                break;
-            case "2":
-                carrito.push(pago2);
-                alert ("En efectivo o transferencia, tenes un 15% de descuento.");                   
-                break; 
-            default:
-                
-        alert ("Ingresa el valor correcto")
-    
-        }
-    } while (condition);
-           
-         
-    }
-    
-    fPagos();
+const pago = [
 
-// total del carrito 
+    { forma: "credito", descuento: 20 },
+    { forma: "debito", descuento: 20 },
+    { forma: "efectivo", descuento: 15 },
+    { forma: "transferencia", descuento: 15 },
 
+];
 
-const total = carrito.reduce((acc, el) => {
+console.log(pago);
 
-    return acc + el.precio;
-    
-    
-    },0);
-    
-    alert ("El sub-total de tu compra es " + total )
-    carrito.push(total);
-    console.log(carrito);
+let ingreso = prompt("Ingresa forma de pago");
 
+function filtrarPagos(arr, filtro) {
+    const elegido = arr.filter ((el)=>{
 
+        return el.forma.includes(filtro);
 
-
-
-//filtrar por precio
-/*
-let porPrecio = prompt(
-    "Selecciona un opción \n 1 - Menor precio \n 2 - Mayor precio"
-  );
-  
-  function filtrarPorPrecio(arr, filtro) {
-    let valor = prompt("Ingresa el monto");
-  
-    return arr.filter((el) => {
-      switch (filtro) {
-        case "1":
-          return el.precio < valor;
-          break;
-        case "2":
-          return el.precio > valor;
-          break;
-      }
     });
-  }
-  const precio = filtrarPorPrecio(productos, porPrecio);
-  console.log(precio);*/
+   return elegido; 
+}
+
+const buscador = filtrarPagos(pago, ingreso.toLocaleLowerCase());
+console.log(buscador);
+console.log(carrito);
 
 
+//para iterar cada elemento
 
+for (const elemento of buscador) {
 
-
-/*
-
-// meidos de pago recargo o descuentos (ver la clase)
-
-
-function Pago(nombre, resultado ) {
-
-    this.nombre = nombre;
-    this.resultado = parseInt(resultado);
-
-    this.sumarRecargo = ()=>{
-
-        pago.push = this.resultado * 1.21;
-       
-        
-    }
-
-   this.restarImp = () => {
- 
-    this.total = this.total - 1.20 ;
+    carrito.push(elemento);
      
-   }
-   
-   
-}
-
-const pagoNuevo1 = new Pago ("tarjeta crédito", 20);
-const pagoNuevo2 = new Pago ("debito", 20);
-const pagoNuevo3 = new Pago ("efectivo", 15);
-
-console.log(resultado);
-function fPagos() {
-    let  pagos = prompt ("Selecciona la forma de pago, ingresando el número correspondiente : \n 1-Crédito \n 2-Debito \n 3-Efectivo o transferencia");
-    
-            switch (pagos) {
-                case "1":
-
-                    carrito.push(pagoNuevo1);
-                    return alert ("En efectivo o transferencia, tenes un 15% de descuento.");
-                    
-    
-                case "2":
-                    carrito.push(pagoNuevo2);
-                   return alert ("Débito, esta opción tiene 20% de recargo.");
-                    
-                 case "3":
-                    carrito.push(pagoNuevo3);
-                   return alert ("Crédito.");
-                 
-                default:
-                    
-            alert ("Ingresa el valor correcto")
-        
-            }
-         
     }
-    
-    fPagos();
 
-
-function crearCarro(Carro) {
-    
-for (const resultado of Carro) {
-    
-    Carro.forEach(element => {
-        let suma = suma + element.precio
-    });
-}
-
-
-alert("el total es : " + suma);
-
-    
-}
-
-
-crearCarro();*/
-
-    
-
-
-
-
-
+console.log(carrito);
 
